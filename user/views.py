e7a7429d4e50a21a4f57717bc82c73daaa94c277
cyb0rg007 <aarav30302001@gmail.com>
2020-04-23 02:40:03 +0530

    1st commit

---
+++ user/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth import login,authenticate
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+def signup(request):
+    if request.method=="POST":
+        form=UserCreationForm(request.POST)
+        if form.is_valid:
+            form.save()
+            username=form.cleaned_data.get('username')
+            password=form.cleaned_data.get('password')
+            users=authenticate(username="username",password="password")
+            print("~~~~~~~~~>>>>",list(request.POST.values()))
+            print("user====>>>",users)
+            
+            messages.success(request, f'Your account has been created for {username}! You are now able to log in')
+            return redirect('home')
+    else:
+        form=UserCreationForm()
+    return render(request,'user/signup.html',{"form":form})
+
+def home(request):
+    return render(request,'user/home.html')
+
+
+def login(request):
+    pass
+    
